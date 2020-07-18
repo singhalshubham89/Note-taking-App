9aca0cd636c81e40db6e8f269283c15d124f8da6
Shubham Singhal <singhalshubham89@gmail.com>
2020-07-18 14:59:22 +0530

    Adding four components basic design

---
+++ src/components/TodoAddNote.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TodoAddNote() {
+  return <div>TodoAddNote</div>;
+}
---
+++ src/components/TodoCalendar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Calendar from "react-calendar";
+
+export default function TodoCalendar() {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+}
